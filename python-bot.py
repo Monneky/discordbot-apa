fe9f0156f4e42d331f280a7412a9fe58afc2be24
Cesar Ivann Llamas <ivllamoficial@gmail.com>
2022-06-22 12:58:27 -0500

    basic interaction with bot and user

--- python-bot.py
+++ python-bot.py
@@ -1,2 +1,19 @@
-# python bot
+## Appa bot
+import discord
 import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+    
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startwith('$hello'):
+        await message.channel.send('hello!')
+        
+client.run(os.getenv('OTg5MjE2NDMyMjg4MDYzNTY4.G2eMNW.XvjwXPA4jU4Qg2wIFPHtl7KHNRXWt43cTq8egU'))
