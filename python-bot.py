924e6dc6e008de3c2ae3de5a24960214ac3e039a
Cesar Ivann Llamas <ivllamoficial@gmail.com>
2022-06-22 13:01:43 -0500

    change the token to .env file

--- python-bot.py
+++ python-bot.py
@@ -16,4 +16,4 @@ async def on_message(message):
     if message.content.startwith('$hello'):
         await message.channel.send('hello!')
         
-client.run(os.getenv('OTg5MjE2NDMyMjg4MDYzNTY4.G2eMNW.XvjwXPA4jU4Qg2wIFPHtl7KHNRXWt43cTq8egU'))
+client.run(os.getenv('TOKEN'))
